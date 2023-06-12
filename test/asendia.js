c502847d50e9ddfbfa6a59b862c0c8d812af0583
Aldin Alagić <aldin@dysordian.com>
2023-06-13 00:54:54 +0200

    Add asendia tracking

---
+++ test/asendia.js
@@ -0,0 +1,27 @@
+/* globals before it describe */
+
+'use strict'
+
+var assert = require('assert')
+
+var prepare = require('./fixtures/prepare')
+var tracker = require('..')
+
+var courier = tracker.courier(tracker.COURIER.ASENDIA.CODE)
+
+describe(tracker.COURIER.ASENDIA.NAME, function () {
+  var deliveredNumber = 'TRACKING-NUMBER'
+
+  before(function () {
+    // @TODO add nock
+    prepare(courier, deliveredNumber)
+  })
+
+  it('delivered number', async function () {
+
+    var result = await courier.trace(deliveredNumber);
+    assert.equal(deliveredNum, result.number)
+    assert.equal(tracker.COURIER.ASENDIA.CODE, result.courier.code)
+    assert.equal(tracker.STATUS.DELIVERED, result.status)
+  })
+})
